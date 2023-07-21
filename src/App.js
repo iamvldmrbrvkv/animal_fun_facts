1c63ec3bba5eff3b7342f3f570d0c6db3805d1de
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-07-22 01:44:44 +0300

    resourses

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import ocean from './images/ocean.jpeg';
+import { animals } from './animals';
 import './App.css';
 
 function App() {
+  const title = '';
+
+  const showBackground = true;
+
+  const background = (
+  <img
+    className='background'
+    alt='ocean'
+    src={ocean} />
+);
+
+const images = [];
+
+const displayFact = e => {
+  const animalName = e.target.alt;
+  const randomIndex = Math.floor(Math.random() * animals[animalName].facts.length);
+  const fact = animals[animalName].facts[randomIndex];
+  document.getElementById('fact').innerHTML = fact;
+}
+
+for (const animal in animals) {
+  images.push(
+    <img
+      key={animal}
+      className='animal'
+      alt={animal}
+      src={animals[animal].image}
+      aria-label={animal}
+      role='button'
+      onClick={displayFact} />
+  );
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{title || 'Click an animal for a fun fact'}</h1>
+      {showBackground && background}
+      <div className='animals'>{images}</div>
+      <p id='fact'></p>
     </div>
   );
 }
---
+++ src/animals.js
@@ -0,0 +1,18 @@
+import dolphinImgSrc from './images/dolphin.jpeg';
+import lobsterImgSrc from './images/lobster.jpeg';
+import starfishImgSrc from './images/starfish.jpeg';
+
+export const animals = {
+    dolphin: {
+        image: dolphinImgSrc,
+        facts: ['Dolphins have been shown to give distinct names to each other!', 'Dolphins are known to display their own culture!', 'Dolphins have two stomachs!']
+      },
+      lobster: {
+        image: lobsterImgSrc,
+        facts: ['Lobsters taste with their legs!', 'Lobsters chew with their stomachs!', 'Lobsters can live as long as 100 years.']
+      },
+      starfish: {
+        image: starfishImgSrc,
+        facts: ['Starfish can have up to 40 arms!', 'Starfish have no brain and no blood!', 'Starfish can regenerate their own arms!']
+      }
+};
